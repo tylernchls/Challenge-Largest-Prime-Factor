6026b58fa5585f468d5bfe0e22bccefc2636cc0c
Tyler Nichols <tylernchls@gmail.com>
2016-12-12 13:29:45 -1000

    joes solution need to review

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,33 @@
-exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
-  // do your work here
+'use strict'
 
-  return primeNumber;
+// A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself
+// Prime factors are prime numbers that divide that integer exactly.
+
+exports.largestPrimeFactor = function( n ) {
+  let lrgestPrimeFactor;
+  let i = 2;
+  while ( i <= n ) {
+    if ( isDivisible( n,  i ) && isPrime( i ) ) {
+      lrgestPrimeFactor = i;
+    }
+    i++;
+  }
+  return lrgestPrimeFactor;
 };
+
+function isPrime( value ) {
+  if ( value === 2 ) {
+    return true;
+  }
+  for ( let i = 2; i < value; i++ ) {
+    if ( value % i === 0 ) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isDivisible ( n, divider ) {
+  return n % divider === 0;
+}
+
